9a5e52a292df20b5005bc8a16659d44906fc460d
Thomas Parisot <thomas.parisot@bbc.co.uk>
2014-07-28 14:55:43 +0100

    `new` cannot be used for an OfflineAudioContext as specific arguments are required.

--- offline.js
+++ offline.js
@@ -3,4 +3,4 @@
 var window = require('global/window');
 
 var Context = window.OfflineAudioContext || window.webkitOfflineAudioContext;
-if (Context) module.exports = new Context;
+if (Context) module.exports = Context;
